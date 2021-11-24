514ede8b59c707d2c1ffe9e53cb08e826035c53c
Celso Vanderlei <celso.vanderlei@promptinf.com.br>
2021-11-24 17:38:09 -0300

    Update 20211124C

--- config/appServer.js
+++ config/appServer.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const consign = require('consign')
 
-export default () => {
+modeule.exports = () => {
     const app = express()
     app.use(express.json())
     app.use((req, res, next) => {
